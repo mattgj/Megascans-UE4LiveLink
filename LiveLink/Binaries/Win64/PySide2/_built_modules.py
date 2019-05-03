20ebcaf05652eaf8296948ce8ff54c799926d28b
Durra <durra@quixel.com>
2019-05-03 16:48:31 +0500

    Fixes around UE 4.22 support

--- LiveLink/Binaries/Win64/PySide2/_built_modules.py
+++
@@ -1,3 +0,0 @@
-built_modules = list(name for name in
-    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;WinExtras;Xml;XmlPatterns;Help;Multimedia;MultimediaWidgets;OpenGL;Qml;Quick;QuickWidgets;Script;ScriptTools;TextToSpeech;Charts;Svg;DataVisualization;UiTools;AxContainer;WebChannel;WebEngineWidgets;WebSockets"
-    .split(";"))
